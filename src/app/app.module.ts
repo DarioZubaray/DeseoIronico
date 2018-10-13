93c25d42fa40c824e57487eb232721d60415612d
dario zubaray <dzubaray@gmail.com>
2018-10-13 20:27:45 -0300

    agregando los componentes pendientes  y terminados base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,18 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { PendientesPage } from '../pages/pendientes/pendientes.component';
+import { TerminadosPage } from '../pages/terminados/terminados.component';
+
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    PendientesPage,
+    TerminadosPage
   ],
   imports: [
     BrowserModule,
@@ -29,7 +34,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    PendientesPage,
+    TerminadosPage
   ],
   providers: [
     StatusBar,
