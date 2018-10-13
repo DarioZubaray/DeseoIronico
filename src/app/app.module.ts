fe1319570143111919e839b589e5eb28cd8748c1
dario zubaray <dzubaray@gmail.com>
2018-10-13 20:33:53 -0300

    removiendo los page home, contact y about remanentes de la platilla tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { NgModule, ErrorHandler } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -17,9 +15,6 @@ import { TerminadosPage } from '../pages/terminados/terminados.component';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
     TabsPage,
     PendientesPage,
     TerminadosPage
@@ -31,9 +26,6 @@ import { TerminadosPage } from '../pages/terminados/terminados.component';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
     TabsPage,
     PendientesPage,
     TerminadosPage
