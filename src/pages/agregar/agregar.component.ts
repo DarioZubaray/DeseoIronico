bbee9bd63e857ff91da1a0b9f22687dbb04326eb
dario zubaray <dzubaray@gmail.com>
2018-10-14 14:47:15 -0300

    añadiendo la funcionalidad de agregar nuevos items a la lista de deseos

--- src/pages/agregar/agregar.component.ts
+++ src/pages/agregar/agregar.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { DeseoServices } from '../../services/deseos.service';
-import { Lista } from '../../models/lista.model';
+import { Lista, ListaItem } from '../../models';
 import { NavParams } from 'ionic-angular';
 
 @Component({
@@ -10,14 +10,29 @@ import { NavParams } from 'ionic-angular';
 export class AgregarPage {
 
   lista: Lista;
+  nombreItem : string = '';
 
   constructor( public deseosServices: DeseoServices, private navParams: NavParams) {
 
     const titulo = this.navParams.get('titulo');
-
     console.log( titulo );
-
     this.lista = new Lista( titulo );
+  }
+
+  agregarItem() {
+    if( this.nombreItem.length === 0){
+      return;
+    }
+    console.log( this.nombreItem );
+
+    const nuevoItem = new ListaItem( this.nombreItem );
+
+    this.lista.items.push(nuevoItem);
+    this.nombreItem = '';
+  }
+
+  actualizarItem( item: ListaItem ) {
+    item.completado = !item.completado;
   }
 
 }
