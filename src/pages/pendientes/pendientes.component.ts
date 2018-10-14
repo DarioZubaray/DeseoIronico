43ed0a0973187780286cf827a9793b9d6d1fcc51
dario zubaray <dzubaray@gmail.com>
2018-10-13 21:21:58 -0300

    modificando el estilo de la aplicacion a un color morado, agregando banner como titulos en las paginas

--- src/pages/pendientes/pendientes.component.ts
+++ src/pages/pendientes/pendientes.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { DeseoServices } from '../../services/deseos.service';
+import { Lista } from '../../models/lista.model';
 
 @Component({
   selector: 'page-pendientes',
@@ -8,6 +9,10 @@ import { DeseoServices } from '../../services/deseos.service';
 export class PendientesPage {
 
   constructor( public deseosServices: DeseoServices) {
-    
+
+  }
+
+  listaSeleccionada(lista: Lista){
+    console.log(lista); 
   }
 }
