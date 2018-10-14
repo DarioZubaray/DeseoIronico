a2965180b0c7de0f38cf931215c16d6ccda662a8
dario zubaray <dzubaray@gmail.com>
2018-10-13 21:03:23 -0300

    agregando 2 lista al inicio del servicio

--- src/services/deseos.service.ts
+++ src/services/deseos.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core'
+import { Lista } from '../models/lista.model';
 
 @Injectable()
 export class DeseoServices{
 
+  listas: Lista[] = [];
+
   constructor() {
-    console.log("Deseo services inicializado");
+    console.log("DeseoService inicializado");
+
+    const lista1 = new Lista('Recolectar Piedras del Infinito');
+    const lista2 = new Lista('Hereos a vencer');
+
+    this.listas.push(lista1, lista2);
+
+    console.log(this.listas);
   }
 }
